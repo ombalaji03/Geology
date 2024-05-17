d5088e3399864508d021091e7da8476bbdd077b9
ombalaji03 <ombalaji2003@gmail.com>
2024-05-18 03:27:13 +0530

    added home page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { base } from "$app/paths";
+    import Heading2 from "$lib/components/headings/Heading2.svelte";
+</script>
+
+<main>
+    <a href="{base}/mineralogy">
+        <Heading2 text="Mineralogy"></Heading2>
+    </a>
+    <a href="{base}/">
+        <Heading2 text="Optical Mineralogy"></Heading2>
+    </a>
+    <a href="{base}/">
+        <Heading2 text="Crystallography"></Heading2>
+    </a>
+</main>
