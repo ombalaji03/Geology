099a7c25fefe423cdf662a75f8c03f60cc99e384
ombalaji03 <ombalaji2003@gmail.com>
2024-05-18 03:35:59 +0530

    added variable heading

---
+++ src/lib/components/headings/HeadingX.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "./headings.css";
+    export let color;
+    export let size;
+    export let text;
+</script>
+
+<div class="mb-4 pl-2 {color} drop-shadow-lg">
+    <h1 class="p-4 {size} font-bold border-[1px] border-[#3b4252]">
+        {text}
+    </h1>
+</div>
