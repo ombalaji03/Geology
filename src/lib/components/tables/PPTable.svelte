885e4b648b53b9ef3c11791727af022847bf8338
ombalaji03 <ombalaji2003@gmail.com>
2024-05-19 00:21:53 +0530

    physical property table component

---
+++ src/lib/components/tables/PPTable.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let mineral = {};
+</script>
+
+<table class="mb-4 table-auto border-separate">
+    {#each Object.entries(mineral) as [property, value]}
+        <tr>
+            <td class="pe-4 text-yellow align-top">{property}</td>
+            <td class="align-top">{value}</td>
+        </tr>
+    {/each}
+</table>
