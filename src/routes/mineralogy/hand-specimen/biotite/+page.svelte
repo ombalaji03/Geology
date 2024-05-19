ad30e6636d2384ee2957c8d5fbaaed9d23d5f167
ombalaji03 <ombalaji2003@gmail.com>
2024-05-19 19:37:53 +0530

    add biotite

---
+++ src/routes/mineralogy/hand-specimen/biotite/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import Heading1 from "$lib/components/headings/Heading1.svelte";
+    import Heading2 from "$lib/components/headings/Heading2.svelte";
+    import PPTable from "$lib/components/tables/PPTable.svelte";
+
+    const biotite = {
+        Color: "black",
+        "Form/Habit": "foliated",
+        Lusture: "vitreous",
+        Streak: "white",
+        Hardness: "2.5-3",
+        Cleavage: "perfect 1 set basal",
+        Fracture: "flaky",
+        "Specific Gravity": "3 (moderate)",
+        "Chemical Composition": "KAl2(AlSi3O10)(OH)2",
+        "Crystal System": "monoclinic",
+        "Diagnostic Properties":
+            "foliated form, hardness, perfect 1 set basal cleavage, flaky fractures",
+    };
+</script>
+
+<main>
+    <Heading1 text="Biotite"></Heading1>
+
+    <Heading2 text="Physical Properties"></Heading2>
+
+    <PPTable mineral={biotite}></PPTable>
+
+    <Heading2 text="Remarks"></Heading2>
+
+    <Heading2 text="Gallery"></Heading2>
+</main>
