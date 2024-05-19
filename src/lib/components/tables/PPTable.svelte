e0c6c33c105b745c6bb65017d9e08ad36bcad1ec
ombalaji03 <ombalaji2003@gmail.com>
2024-05-19 19:13:22 +0530

    change property font style

--- src/lib/components/tables/PPTable.svelte
+++ src/lib/components/tables/PPTable.svelte
@@ -5,7 +5,7 @@
 <table class="mb-4 table-auto border-separate">
     {#each Object.entries(mineral) as [property, value]}
         <tr>
-            <td class="pe-4 text-yellow align-top">{property}</td>
+            <td class="pe-4 text-yellow font-semibold align-top">{property}</td>
             <td class="align-top">{value}</td>
         </tr>
     {/each}
