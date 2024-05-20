115e64ce0f5cb12d5a30af66e6ec0c1b40748d44
ombalaji03 <ombalaji2003@gmail.com>
2024-05-20 21:50:46 +0530

    added ppl and xpl props

--- src/routes/optical-mineralogy/thin-sections/template.svelte
+++ src/routes/optical-mineralogy/thin-sections/template.svelte
@@ -3,6 +3,23 @@
     import Heading2 from "$lib/components/headings/Heading2.svelte";
     import Heading3 from "$lib/components/headings/Heading3.svelte";
     import PropTable from "$lib/components/tables/PropTable.svelte";
+
+    const pplProperties = {
+        Form: "FORM",
+        Color: "COLOR",
+        Pleochroism: "PLEOCHROISM",
+        Relief: "RELIEF",
+        Cleavage: "CLEAVAGE",
+        Fracture: "FRACTURE",
+        Twinkling: "TWINKLING",
+    };
+
+    const xplProperties = {
+        "Isotropic/Anisotropic": "ISO_ANISO",
+        "Interference Color": "INTERFERENCE_COLOR",
+        "Extinction Angle": "EXTINCTION_ANGLE",
+        Twinning: "TWINNING",
+    };
 </script>
 
 <main>
@@ -12,7 +29,11 @@
 
     <Heading3 text="In PPL"></Heading3>
 
+    <PropTable properties={pplProperties}></PropTable>
+
     <Heading3 text="In XPL"></Heading3>
+
+    <PropTable properties={xplProperties}></PropTable>
 
     <Heading2 text="Gallery"></Heading2>
 </main>
