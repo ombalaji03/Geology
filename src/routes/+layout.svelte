5e8314d9c21c7d1baeb40a7b22ca77c60a694999
ombalaji03 <ombalaji2003@gmail.com>
2024-05-21 00:06:18 +0530

    added back button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,31 @@
 <script>
-    // import { base } from "$app/paths";
+    import { base } from "$app/paths";
     import "../app.css";
 
-    const base = process.argv.includes("dev") ? "" : "/geology";
+    const homeURL = process.argv.includes("dev") ? "" : "/geology";
+    const backBtnStr = "Back";
 </script>
 
+<!-- Navigation Bar -->
 <nav
     class="mb-0 p-4 bg-dark0 text-2xl font-bold border-b-[1px] border-dark1 drop-shadow-lg"
 >
-    <a href="{base}/">Geology</a>
+    <a href="{homeURL}/">Geology</a>
 </nav>
+
+<!-- Back Button -->
+<a href="{base}/">
+    <div
+        class="mt-4 py-2 px-4 inline-block bg-dark0 border-[1px] border-dark1 drop-shadow-lg border-l-0"
+    >
+        {backBtnStr}
+    </div>
+</a>
 
 <slot />
 
 <style>
+    div,
     nav {
         font-family: "Rubik", sans-serif;
     }
