50a377468c118184256fd8bcc8490a91ebfc2951
ombalaji03 <ombalaji2003@gmail.com>
2024-05-20 21:51:00 +0530

    fix broken export

--- src/lib/components/tables/PropTable.svelte
+++ src/lib/components/tables/PropTable.svelte
@@ -1,9 +1,9 @@
 <script>
-    export let mineral = {};
+    export let properties = {};
 </script>
 
 <table class="mb-4 table-auto border-separate">
-    {#each Object.entries(mineral) as [property, value]}
+    {#each Object.entries(properties) as [property, value]}
         <tr>
             <td class="pe-4 text-yellow font-semibold align-top">{property}</td>
             <td class="align-top">{value}</td>
