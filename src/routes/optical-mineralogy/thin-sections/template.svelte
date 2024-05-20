235c917c27d5bf5c6088dcceb516119a009a26ce
ombalaji03 <ombalaji2003@gmail.com>
2024-05-20 21:39:31 +0530

    added a template for thin section pages

---
+++ src/routes/optical-mineralogy/thin-sections/template.svelte
@@ -0,0 +1,18 @@
+<script>
+    import Heading1 from "$lib/components/headings/Heading1.svelte";
+    import Heading2 from "$lib/components/headings/Heading2.svelte";
+    import Heading3 from "$lib/components/headings/Heading3.svelte";
+    import PropTable from "$lib/components/tables/PropTable.svelte";
+</script>
+
+<main>
+    <Heading1 text="MINERAL_NAME"></Heading1>
+
+    <Heading2 text="Optical Properties"></Heading2>
+
+    <Heading3 text="In PPL"></Heading3>
+
+    <Heading3 text="In XPL"></Heading3>
+
+    <Heading2 text="Gallery"></Heading2>
+</main>
