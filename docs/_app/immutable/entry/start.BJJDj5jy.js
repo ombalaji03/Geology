6b6ba120883d6a0e4b8022beea57206ac540d04d
ombalaji03 <ombalaji2003@gmail.com>
2024-05-21 13:40:50 +0530

    back btn more work

---
+++ docs/_app/immutable/entry/start.BJJDj5jy.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DCO-wxcD.js";export{t as start};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,18 +11,15 @@
 </nav>
 
 <!-- Back Button -->
-<a href="{base}/">
-    <div
-        class="mt-4 py-2 px-4 inline-block bg-dark0 border-[1px] border-dark1 drop-shadow-lg border-l-0"
-    >
-        Back
-    </div>
-</a>
+<nav
+    class="mt-4 py-2 px-4 inline-block bg-dark0 border-[1px] border-dark1 drop-shadow-lg border-l-0"
+>
+    <a href="{base}/"> Back </a>
+</nav>
 
 <slot />
 
 <style>
-    div,
     nav {
         font-family: "Rubik", sans-serif;
     }
