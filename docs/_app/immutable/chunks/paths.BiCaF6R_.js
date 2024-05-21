8a5b3e6a2dd4788cecf61c15222faffa9883a885
ombalaji03 <ombalaji2003@gmail.com>
2024-05-21 14:03:25 +0530

    back

--- docs/_app/immutable/chunks/paths.BiCaF6R_.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_2ym3th)==null?void 0:s.base)??"/geology";var t;const a=((t=globalThis.__sveltekit_2ym3th)==null?void 0:t.assets)??e;export{a,e as b};
--- docs/_app/immutable/entry/start.C5o_beFE.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.DOGEmv4D.js";export{t as start};
