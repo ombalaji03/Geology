7a083bcc06f88ab931bff41c39043aa42d9440c7
ombalaji03 <ombalaji2003@gmail.com>
2024-06-08 13:59:20 +0530

    update quartz

--- src/routes/mineralogy/hand-specimen/quartz/+page.svelte
+++ src/routes/mineralogy/hand-specimen/quartz/+page.svelte
@@ -1,21 +1,23 @@
 <script>
+    import Paragraph from "$lib/components/Paragraph.svelte";
     import Heading1 from "$lib/components/headings/Heading1.svelte";
     import Heading2 from "$lib/components/headings/Heading2.svelte";
     import PropTable from "$lib/components/tables/PropTable.svelte";
 
     const quartz = {
-        Color: "white",
-        "Form/Habit": "prismatic",
-        Lusture: "vitreous",
-        Streak: "white",
+        Color: "Colorless, white, milky",
+        "Form/Habit":
+            "Prismatic or massive. Common crystals are six sided prisms terminated by rhombohedrons, which may appear to be six sided hexagonal dipyramids.",
+        Lusture: "Vitreous",
+        Streak: "White (indeterminable)",
         Hardness: "7",
-        Cleavage: "absent",
-        Fracture: "conchoidal",
-        "Specific Gravity": "low",
+        Cleavage: "Absent",
+        Fracture: "Conchoidal",
+        "Specific Gravity": "2.65",
         "Chemical Composition": "SiO2",
-        "Crystal System": "hexagonal",
+        "Crystal System": "Hexagonal (rhombohedral)",
         "Diagnostic Properties":
-            "hardness of 7, absence of cleavage, conchoidal fractures",
+            "Hardness of 7, absence of cleavage, conchoidal fractures, prismatic habit",
     };
 </script>
 
@@ -26,7 +28,29 @@
 
     <PropTable properties={quartz}></PropTable>
 
-    <Heading2 text="Remarks"></Heading2>
+    <Heading2 text="Associations"></Heading2>
+
+    <Paragraph
+        text="Quartz is a common and essential mineral in many igneous, sedimentary and metamorphic rocks. It occurs in all silicic metamorphic and igneous rocks."
+    ></Paragraph>
 
     <Heading2 text="Gallery"></Heading2>
+
+    <img
+        class="mb-4"
+        src="https://www.lillypadvillage.com/wp-content/uploads/2017/01/Crystal-Quartz-980x980.jpg"
+        alt="Quartz crystals with prismatic habit."
+    />
+
+    <img
+        class="mb-4"
+        src="https://cdn.shopify.com/s/files/1/2164/6515/files/10facts1_grande.jpg?v=1520393303"
+        alt="Quartz crystal with massive habit"
+    />
 </main>
+
+<style>
+    img {
+        width: 100%;
+    }
+</style>
